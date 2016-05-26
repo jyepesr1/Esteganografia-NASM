13293eacf2055c93a7709167ad100ece51d83675
jyepesr1 <johanyepes@hotmail.com>
2016-05-25 22:40:42 -0500

    Calculating FileSize

--- hidemsg.asm
+++ hidemsg.asm
@@ -1,5 +1,28 @@
 %include    'functions.asm'
 
+%define sizeof(x) x %+ _size
+
+struc STAT
+    .st_dev:        resd 1
+    .st_ino:        resd 1
+    .st_mode:       resw 1
+    .st_nlink:      resw 1
+    .st_uid:        resw 1
+    .st_gid:        resw 1
+    .st_rdev:       resd 1
+    .st_size:       resd 1
+    .st_blksize:    resd 1
+    .st_blocks:     resd 1
+    .st_atime:      resd 1
+    .st_atime_nsec: resd 1
+    .st_mtime:      resd 1
+    .st_mtime_nsec: resd 1
+    .st_ctime:      resd 1
+    .st_ctime_nsec: resd 1
+    .unused4:       resd 1
+    .unused5:       resd 1
+endstruc
+
 section .data
 
 numArgs           equ   6
@@ -8,12 +31,14 @@ ARCHIVO.out",0xa ; 0xa es salto de línea, 10 en ASCII decimal
 MSGError1Len      equ   $ - MSGError1  ; tamaño del mensaje
 flag1             db    "-f"
 flag1Len          equ   $ - flag1
-imageLen          equ   5242880 ; 5MB
 
 section .bss
 
+stat              resb  sizeof(STAT)
 image             resb  5242880 ; 5MB
 imageName         resb  1024
+imageLen          resb  1024 ; 5MB
+outFileName       resb  1024
 message           resb  1024
 messageLen        resb  1024
 fd_in             resb  1
@@ -39,12 +64,6 @@ mensaje:
    call strLen ; obtengo el tamaño del mensaje
    mov [messageLen],eax
 
-  ; mov eax,4
-  ;mov ebx,1
-  ; mov ecx,[message]
-  ; mov edx,[messageLen]
-  ; int 80h
-  ; call exit
 
 parameter1:
    pop ecx ; Obtengo la primer flag -f
@@ -52,7 +71,7 @@ parameter1:
    mov eax,ecx
    call strLen
    cmp eax,2
-   jg errorParams
+   jne errorParams
 
    cmp byte[ecx],'-'
    jne errorParams
@@ -63,9 +82,37 @@ parameter1:
 parameter2:
    pop ebx ; Obtengo el nombre de la imagen
 
-;   mov [imageName],ebx
+   mov [imageName],ebx
+
+   mov eax,106 ; sys_newstat
+   mov ebx,[imageName]
+   mov ecx,stat
+   int 80h
+
+   mov eax,[stat + STAT.st_size] ; El tamaño del archivo lo podemos hallar accediendo al star mas un offset de 20, pero por legibilidad lo hacemos así
+   mov [imageLen],eax
+
+
+parameter3:
+   pop ecx
+
+   mov eax,ecx
+   call strLen
+   cmp eax,2
+   jne errorParams
+
+   cmp byte[ecx],'-'
+   jne errorParams
+   cmp byte[ecx+1],'o'
+   jne errorParams
+
+
+parameter4:
+   pop eax
+   mov [outFileName],eax
 
    mov eax,5 ; sys_open()
+   mov ebx,[imageName]
    mov ecx,0 ; solo para acceso de lectura
    mov edx,0777 ; Permisos de lectura, escritura y ejecución para todos rwx
    int 80h
@@ -75,32 +122,16 @@ parameter2:
    mov eax,3 ; sys_read()
    mov ebx,[fd_in] ; el descriptor del archivo que abrimos
    mov ecx,image ; el apuntador donde guardaremos el contenido de la imagen
-   mov edx,imageLen ; la cantidad de pixeles que necesitamos
+   mov edx,[imageLen] ; la cantidad de bytes que necesitamos
    int 80h
 
    mov eax,6 ; sys_close()
    mov ebx,[fd_in]
    int 80h
 
-parameter3:
-   pop ecx
-
-   mov eax,ecx
-   call strLen
-   cmp eax,2
-   jg errorParams
-
-   cmp byte[ecx],'-'
-   jne errorParams
-   cmp byte[ecx+1],'o'
-   jne errorParams
-
-
-parameter4:
-   pop ebx
-
    mov eax,8  ; sys_create()
-   mov ecx,0777
+   mov ebx,[outFileName]
+   mov ecx,0420 ; 644 octal -rw-r--r--
    int 80h
 
    mov [fd_out],eax
@@ -108,12 +139,13 @@ parameter4:
    mov eax,4  ; sys_write()
    mov ebx,[fd_out]
    mov ecx,image
-   mov edx,imageLen
+   mov edx,[imageLen]
    int 80h
 
    mov eax,6 ; sys_close()
    mov ebx,[fd_out]
    int 80h
+
 
    jmp exit
 
